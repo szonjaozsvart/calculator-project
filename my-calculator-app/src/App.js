8adc709ffeef425b18b98c5eadc2a2b00c50c5a9
szonjaozsvart <szonjaozsvart@gmail.com>
2022-08-28 18:44:55 +0200

    cleaning & basic

---
+++ my-calculator-app/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [result, setResult] = useState("");
+  return (
+    <div className="wrapper">
+      <form>
+        <input className="result" type="text" value={result} />
+      </form>
+
+      <div className="buttons">
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button>&divide;</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>&ndash;</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button>0</button>
+        <button>,</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
